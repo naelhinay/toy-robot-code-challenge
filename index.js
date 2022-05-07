18379aa0d9634c3be1e2856d81ea7dd3aa8fe983
Nael Hinay <nael.hinay@gmail.com>
2022-05-08 00:14:22 +0800

    feature: add test data

--- index.js
+++ index.js
@@ -1,2 +1,62 @@
-import robotGame from "./robotGame";
+const robotGame = require('./robotGame');
+const testData = require('./testData/sample.json');
 
+const cb = (err, resp) => {
+  if (err) {
+    console.log(err);
+  }
+
+  if (!err && resp) {
+    console.log(resp);
+  }
+};
+
+const getValues = (command) => {
+  const re = /^PLACE[ ]{1,}[0-4],[0-4],(NORTH|EAST|SOUTH|WEST)[ ]{0,}/g;
+  const isValid = re.test(command);
+
+  if (!isValid) {
+    return {
+      x: null,
+      y: null,
+      f: null,
+      isValid: false,
+    };
+  }
+  const c = command.split(' ');
+  const values = c[1].split(',');
+
+  return {
+    x: Number(values[0]),
+    y: Number(values[1]),
+    f: values[2],
+    isValid: true,
+  };
+};
+
+testData.forEach((data) => {
+  switch (data.command) {
+    case 'MOVE':
+      robotGame.actions.move(cb);
+      break;
+    case 'LEFT':
+      robotGame.actions.left(cb);
+      break;
+    case 'RIGHT':
+      robotGame.actions.right(cb);
+      break;
+    case 'REPORT':
+      robotGame.actions.report(cb);
+      break;
+    default:
+      const { x, y, f, isValid } = getValues(data.command); // eslint-disable-line
+
+      if (isValid) {
+        robotGame.actions.place(x, y, f, cb);
+      } else {
+        console.log(`${data.command} is not a valid command`);
+      }
+
+      break;
+  }
+});
--- robotGame.js
+++ robotGame.js
@@ -1,64 +1,62 @@
 const table = {
-  maxX: 5,
-  maxY: 5,
+  maxX: 4, // starts at 0
+  maxY: 4, // starts at 0
 };
 const allowedDirections = ['NORTH', 'EAST', 'SOUTH', 'WEST'];
-const robotGame = {
-  robot: {
-    posX: 0,
-    posY: 0,
-    direction: 'NORTH',
-    placed: false,
-  },
-
+const robot = {
+  posX: 0,
+  posY: 0,
+  direction: 'NORTH',
+  placed: false,
+};
+const actions = {
   place: (x, y, f, cb) => {
     if (!allowedDirections.includes(f)) {
-      console.log('Invalid direction, ignoring');
       return cb('Error: invalid direction');
     }
 
     if (x < 0 || y < 0 || x > table.maxX || y > table.maxY) {
-      console.log('Invalid position, ignoring.');
       return cb('Error: invalid position');
     }
 
-    this.robot.posX = x;
-    this.robot.poxY = y;
-    this.robot.direction = f;
-    this.robot.placed = true;
+    console.log('=====================');
+    console.log(`PLACE ${x},${y},${f}`);
+    robot.posX = x;
+    robot.posY = y;
+    robot.direction = f;
+    robot.placed = true;
 
-    return cb(null, 'Robot placed');
+    return cb(null);
   },
 
   move: (cb) => {
-    if (!this.robot.placed) {
-      console.log('robot not placed');
+    if (!robot.placed) {
       return cb('Error: robot not placed');
     }
 
     let moved = false;
-    switch (this.robot.direction) {
+    switch (robot.direction) {
       case 'NORTH':
-        if (this.robot.posY >= 0 && this.robot.posY < table.maxY) {
-          this.robot.posY += 1;
+        if (robot.posY >= 0 && robot.posY < table.maxY) {
+          robot.posY += 1;
         }
         moved = true;
         break;
       case 'SOUTH':
-        if (this.robot.posY <= table.maxY && this.robot.posY > 0) {
-          this.robot.posY -= 1;
+        if (robot.posY <= table.maxY && robot.posY > 0) {
+          robot.posY -= 1;
         }
         moved = true;
         break;
       case 'EAST':
-        if (this.robot.posX >= 0 && this.robot.posX < table.maxX) {
-          this.robot.posX += 1;
+        if (robot.posX >= 0 && robot.posX < table.maxX) {
+          robot.posX += 1;
         }
         moved = true;
         break;
       case 'WEST':
-        if (this.robot.posX <= table.maxX && this.robot.posX > 0) {
-          this.robot.posY -= 1;
+        if (robot.posX <= table.maxX && robot.posX > 0) {
+          robot.posY -= 1;
         }
         moved = true;
         break;
@@ -71,73 +69,74 @@ const robotGame = {
       return cb('Error: robot not moved');
     }
 
-    return cb(null, `Robot moved ${this.robot.direction}`);
+    console.log('MOVE');
+    return cb(null);
   },
 
   left: (cb) => {
-    if (!this.robot.placed) {
-      console.log('robot not placed');
+    if (!robot.placed) {
       return cb('Error: robot not placed');
     }
 
-    const oldDirection = this.robot.direction;
-    switch (this.robot.direction) {
+    switch (robot.direction) {
       case 'NORTH':
-        this.robot.direction = 'WEST';
+        robot.direction = 'WEST';
         break;
       case 'SOUTH':
-        this.robot.direction = 'EAST';
+        robot.direction = 'EAST';
         break;
       case 'EAST':
-        this.robot.direction = 'NORTH';
+        robot.direction = 'NORTH';
         break;
       case 'WEST':
-        this.robot.direction = 'SOUTH';
+        robot.direction = 'SOUTH';
         break;
       default:
         // do nothing;
         break;
     }
 
-    return cb(null, `Robot rotated from ${oldDirection} to ${this.robot.direction}`);
+    console.log('LEFT');
+    return cb(null);
   },
 
   right: (cb) => {
-    if (!this.robot.placed) {
-      console.log('robot not placed');
+    if (!robot.placed) {
       return cb('Error: robot not placed');
     }
 
-    const oldDirection = this.robot.direction;
-    switch (this.robot.direction) {
+    switch (robot.direction) {
       case 'NORTH':
-        this.robot.direction = 'EAST';
+        robot.direction = 'EAST';
         break;
       case 'SOUTH':
-        this.robot.direction = 'WEST';
+        robot.direction = 'WEST';
         break;
       case 'EAST':
-        this.robot.direction = 'SOUTH';
+        robot.direction = 'SOUTH';
         break;
       case 'WEST':
-        this.robot.direction = 'NORTH';
+        robot.direction = 'NORTH';
         break;
       default:
         // do nothing;
         break;
     }
 
-    return cb(null, `Robot rotated from ${oldDirection} to ${this.robot.direction}`);
+    console.log('RIGHT');
+    return cb(null);
   },
 
   report: (cb) => {
-    if (!this.robot.placed) {
-      console.log('robot not placed');
+    if (!robot.placed) {
       return cb('Error: robot not placed');
     }
 
-    return cb(null, `x: ${this.robot.posX}, y: ${this.robot.posY}, f: ${this.robot.direction}`);
+    console.log('REPORT');
+    return cb(null, `Output: ${robot.posX},${robot.posY},${robot.direction}`);
   },
 };
 
-export default robotGame;
+module.exports = {
+  actions,
+};
